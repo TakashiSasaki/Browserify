8332950c2d6f705c76d313cd9d0bda0860138cdc
Takashi SASAKI <takashi316@gmail.com>
2018-04-16 10:27:11 +0900

    enrich entryYesTest

--- entryMain.js
+++ entryMain.js
@@ -1,3 +1,24 @@
 console.log("entryMain.js\t: start");
+
+console.log("entryMain.js\t: requiring ./modules/hello.js");
+helloRequired = require("./modules/hello.js");
+console.log("entryMain.js\t: typeof hello = " + typeof hello);
+console.log("entryMain.js\t: typeof helloRequired = " + typeof helloRequired);
+console.log("entryMain.js\t: keys of helloRequired = " + Object.keys(helloRequired));
+
+console.log("entryMain.js\t: requiring ./modules/goodbye.js");
+goodbyeRequired = require("./modules/goodbye.js");
+console.log("entryMain.js\t: typeof goodbye = " + typeof goodbye);
+console.log("entryMain.js\t: typeof goodbyeRequired = " + typeof goodbyeRequired);
+console.log("entryMain.js\t: keys of goodbyeRequired = " + Object.keys(goodbyeRequired));
+
+function entryMainFunction(){
+  console.log("entryMainFunction:");
+  console.log("entryMain.js#entryMainFunction\t: helloRequired.hello()");
+  helloRequired.hello();
+  console.log("entryMain.js#entryMainFunction\t: goodbyeRequired.goodbye()");
+  goodbyeRequiredgoodbye();
+}
+
 console.log("entryMain.js\t: end");
 
--- entryYesTest.js
+++ entryYesTest.js
@@ -1,3 +1,16 @@
 console.log("entryYesTest.js\t: start");
+console.log("entryYesTest.js\t: require ./entryYes");
+entryYesRequired = require("./entryYes");
+console.log("entryYesTest.js\t: typeof entryYesRequired = " + typeof entryYesRequired);
+console.log("entryYesTest.js\t: entryYesRequired has " + Object.keys(entryYesRequired));
+console.log("entryYesTest.js\t: typeof hello = " + typeof hello); 
+console.log("entryYesTest.js\t: typeof helloRequired = " + typeof helloRequired); 
+console.log("entryYesTest.js\t: helloRequired has " + Object.keys(helloRequired)); 
+console.log("entryYesTest.js\t: helloRequired.hello()");
+helloRequired.hello();
+console.log("entryYesTest.js\t: typeof goodbyeRequired = " + typeof goodbyeRequired); 
+console.log("entryYesTest.js\t: goodbyeRequired has " + Object.keys(goodbyeRequired)); 
+console.log("entryYesTest.js\t: goodbyeRequired.goodbye()");
+goodbyeRequired.goodbye();
 console.log("entryYesTest.js\t: end");
 
