9aa34e73962b4c763c92f47b1e081f25a75b5e24
Takashi SASAKI <takashi316@gmail.com>
2018-04-24 23:12:34 +0900

    checkout 8f8c24 node/node.js

--- node/node.js
+++ node/node.js
@@ -1,19 +1,29 @@
-const log = console.log;
+const log = console.log
 
-log("start");
+log("node.js\t: start");
 
 //log("require " + process.argv[2]);
 
-z = require(process.argv[2]);
-log("node.js\t: z = require('" + process.argv[2] + "')");
-log("node.js\t: typeof z = " + typeof requireResult);
-log("node.js\t: keys of z = " + JSON.stringify(Object.keys(requireResult)));
+log("node.js\t: typeof requrie = " + typeof require);
+
+if(typeof process !== "undefined") {
+  log("node.js\t: x = require('" + process.argv[2] + "')");
+  x = require(process.argv[2]);
+  log("node.js\t: typeof x = " + typeof x);
+  log("node.js\t: keys of x = " + JSON.stringify(Object.keys(x)));
+}//if
 
 log("node.js\t: typeof h = " + typeof h);
 log("node.js\t: typeof g = " + typeof g);
 log("node.js\t: typeof mainFunction = " + typeof mainFunction);
 log("node.js\t: typeof hoge = " + typeof hoge);
+if(typeof hoge === "object") {
+  log("node.js\t: keys of hoge = " + JSON.stringify(Object.keys(hoge)));
+}//if
+
 log("node.js\t: typeof module = " + typeof module);
-log("node.js\t: typeof module.exports = " + typeof module.exports);
+if(typeof module === "object!") {
+  log("node.js\t: typeof module.exports = " + typeof module.exports);
+}//if
 log("node.js\t: end");
 
