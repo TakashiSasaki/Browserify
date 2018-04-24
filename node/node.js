5c60cd47cbbd16e984a1c1f75f21967028e8e85a
Takashi SASAKI <takashi316@gmail.com>
2018-04-24 12:26:55 +0900

    log messages in one object in chrome.js

--- node/node.js
+++ node/node.js
@@ -10,11 +10,11 @@ x = require(process.argv[2]);
 log("node.js\t: typeof x = " + typeof x);
 log("node.js\t: keys of x = " + JSON.stringify(Object.keys(x)));
 
-log("node.js\t: h = " + typeof h);
-log("node.js\t: g = " + typeof g);
-log("node.js\t: mainFunction = " + typeof mainFunction);
-log("node.js\t: hoge = " + typeof hoge);
-log("node.js\t: module = " + typeof module);
-log("node.js\t: module.exports = " + typeof module.exports);
+log("node.js\t: typeof h = " + typeof h);
+log("node.js\t: typeof g = " + typeof g);
+log("node.js\t: typeof mainFunction = " + typeof mainFunction);
+log("node.js\t: typeof hoge = " + typeof hoge);
+log("node.js\t: typeof module = " + typeof module);
+log("node.js\t: typeof module.exports = " + typeof module.exports);
 log("node.js\t: end");
 
