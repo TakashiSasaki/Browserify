8f8c248c7a5dc04869fc0e9eccd244204ddbf859
Takashi SASAKI <takashi316@gmail.com>
2018-04-24 14:10:13 +0900

    chrome.js is obsoleted.

--- node/node.js
+++ node/node.js
@@ -4,17 +4,26 @@ log("node.js\t: start");
 
 //log("require " + process.argv[2]);
 
-log("node.js\t: x = require('" + process.argv[2] + "')");
-x = require(process.argv[2]);
+log("node.js\t: typeof requrie = " + typeof require);
 
-log("node.js\t: typeof x = " + typeof x);
-log("node.js\t: keys of x = " + JSON.stringify(Object.keys(x)));
+if(typeof process !== "undefined") {
+  log("node.js\t: x = require('" + process.argv[2] + "')");
+  x = require(process.argv[2]);
+  log("node.js\t: typeof x = " + typeof x);
+  log("node.js\t: keys of x = " + JSON.stringify(Object.keys(x)));
+}//if
 
 log("node.js\t: typeof h = " + typeof h);
 log("node.js\t: typeof g = " + typeof g);
 log("node.js\t: typeof mainFunction = " + typeof mainFunction);
 log("node.js\t: typeof hoge = " + typeof hoge);
+if(typeof hoge === "object") {
+  log("node.js\t: keys of hoge = " + JSON.stringify(Object.keys(hoge)));
+}//if
+
 log("node.js\t: typeof module = " + typeof module);
-log("node.js\t: typeof module.exports = " + typeof module.exports);
+if(typeof module === "object!") {
+  log("node.js\t: typeof module.exports = " + typeof module.exports);
+}//if
 log("node.js\t: end");
 
